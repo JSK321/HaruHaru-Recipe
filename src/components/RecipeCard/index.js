3a39ec164e42c0f4b42a6d3cb7fec4e696d6e3e1
Jae Kim <jaeshinkim321@gmail.com>
2021-01-27 13:08:48 -0800

    css changes to recipe page

--- src/components/RecipeCard/index.js
+++ src/components/RecipeCard/index.js
@@ -1,41 +1,60 @@
 import { Card } from 'react-bootstrap'
 import { ListGroup } from 'react-bootstrap'
-
+import { Accordion } from 'react-bootstrap'
+import './styles.css'
 
 export default function RecipeCard(props) {
     return (
-        <Card className="SignInCard">
-            <Card.Header style={{ textAlign: "center" }}><strong>{props.recipeName}</strong></Card.Header>
-            <Card.Img variant="top" src="holder.js/100px180?text=Image cap" />
-            <Card.Body>
-                <Card.Text>
-                    {props.recipeDescript}
-                </Card.Text>
-            </Card.Body>
-            <ListGroup className="list-group-flush">
-                <Card.Title style={{ textAlign: "center" }}>Ingredients</Card.Title>
-                      {props.ingredients !== null ?
-                            props.ingredients.map(item => (
-                                <ListGroup variant="flush" style={{ textAlign: "center" }}>
-                                    <ListGroup.Item>
-                                        <strong>
-                                        {item.ingredientQuant} {item.ingredientUnit}, {item.ingredient}
-                                        </strong>
-                                    </ListGroup.Item>
-                                </ListGroup>
-                            ))
-                            : null}
-            </ListGroup>
-            <Card.Title style={{ textAlign: "center" }}>Directions</Card.Title>
-            <Card.Body>
-                <Card.Text>
-                    {props.directions}
-                </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-                <Card.Link href="#">Card Link</Card.Link>
-                <Card.Link href="#">Another Link</Card.Link>
-            </Card.Footer>
-        </Card>
+        <Accordion defaultActiveKey="0">
+            <Card className="RecipeCard" style={{ marginTop: "25px" }}>
+                <Card.Header style={{ textAlign: "center" }}><strong>{props.recipeName}</strong></Card.Header>
+                <Card>
+                    <Card.Img
+                        variant="top"
+                        src={props.recipeImage}
+                        className="RecipeCardImage"
+                    />
+                </Card>
+                <Card.Body>
+                    <Card.Text>
+                        {props.recipeDescript}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+            <Card className="RecipeCard">
+                <Accordion.Toggle as={Card.Header} eventKey="0">
+                    <strong>Ingredients</strong>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="0">
+                    <Card.Body>
+                        <ListGroup className="list-group-flush">
+                            {props.ingredients !== null ?
+                                props.ingredients.map(item => (
+                                    <ListGroup variant="flush">
+                                        <ListGroup.Item>
+                                            {item.ingredientQuant} {item.ingredientUnit} {item.ingredient}
+                                        </ListGroup.Item>
+                                    </ListGroup>
+                                ))
+                                : null}
+                        </ListGroup>
+                    </Card.Body>
+                </Accordion.Collapse>
+            </Card>
+            <Card className="RecipeCard">
+                <Accordion.Toggle as={Card.Header} eventKey="1">
+                    <strong>Directions</strong>
+                </Accordion.Toggle>
+                <Accordion.Collapse eventKey="1">
+                    <Card.Body>
+                        {props.directions}
+                    </Card.Body>
+                </Accordion.Collapse>
+                <Card.Footer>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Footer>
+            </Card>
+        </Accordion >
     )
 }
